bab9ea8e3af5140d38bb8bbec2f1abdff0eb78e3
bourekas <45741938+bourekas@users.noreply.github.com>
2024-10-28 15:52:42 +0200

    Add roboto font

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 export const metadata = {
   title: "TPR Game",
   description: "TPR Game by Jeremy Steinberg",
