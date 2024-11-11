a6bce5118f4af7d2822127da14ea7a0799625a22
bourekas <45741938+bourekas@users.noreply.github.com>
2024-11-11 17:30:03 +0200

    Add css baseline

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./globals.css";
 
 export const metadata = {
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <CssBaseline />
         <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
       </body>
     </html>
