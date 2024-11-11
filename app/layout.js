76bb483cc78166bbf660cfc7a750153d143963dc
bourekas <45741938+bourekas@users.noreply.github.com>
2024-11-11 16:54:51 +0200

    Extract roboto fonts to global css

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,5 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import "./globals.css";
 
 export const metadata = {
   title: "TPR Game",
