a831ce26d70015e7fbcfb2dd4b751fae48cd515a
bourekas <45741938+bourekas@users.noreply.github.com>
2024-11-14 15:35:25 +0200

    Refactor  setup-menu

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,7 @@ const config = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/addon-themes"
+    "@storybook/addon-themes",
   ],
   framework: {
     name: "@storybook/nextjs",
--- app/components/setup-menu/setup-menu.js
+++ app/components/setup-menu/setup-menu.js
@@ -7,47 +7,73 @@ import Typography from "@mui/material/Typography";
 import Slider from "@mui/material/Slider";
 import Button from "@mui/material/Button";
 
-const displayTimeSliderMarks = [];
-
-for (let i = 1; i <= 10; i++) {
-  displayTimeSliderMarks.push({ value: i, label: i });
-}
-
 export default function SetupMenu({
   initialDisplayTime = 5,
   headingLevel = 1,
   onStart,
 }) {
   const [displayTime, setDisplayTime] = useState(initialDisplayTime);
-  const handleDisplayTimeChange = (event, newTime) => setDisplayTime(newTime);
+  const handleDisplayTimeChange = (newTime) => setDisplayTime(newTime);
+  const handleStart = () => onStart({ displayTime });
 
   return (
     <Paper elevation={3} sx={{ padding: 3 }}>
       <Box sx={{ textAlign: "center" }}>
-        <Typography component={`h${headingLevel}`} variant="h4">
-          TPR Game Setup
-        </Typography>
+        <SetupHeading headingLevel={headingLevel} />
       </Box>
       <Box sx={{ mb: 2 }}>
-        <Typography id="word-display-time" variant="subtitle1" gutterBottom>
-          Display time for each word: {displayTime} second(s)
-        </Typography>
-        <Slider
-          aria-label="Display time"
-          aria-describedby="word-display-time"
-          min={0}
-          max={10}
-          step={null}
-          marks={displayTimeSliderMarks}
-          value={displayTime}
-          onChange={handleDisplayTimeChange}
+        <DisplayTimeOption
+          displayTime={displayTime}
+          onDisplayTimeChange={handleDisplayTimeChange}
         />
       </Box>
       <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Button variant="contained" onClick={() => onStart({ displayTime })}>
-          Start
-        </Button>
+        <StartButton onStart={handleStart} />
       </Box>
     </Paper>
+  );
+}
+
+function SetupHeading({ headingLevel }) {
+  return (
+    <Typography component={`h${headingLevel}`} variant="h4">
+      TPR Game Setup
+    </Typography>
+  );
+}
+
+const displayTimeSliderMarks = [];
+
+for (let i = 1; i <= 10; i++) {
+  displayTimeSliderMarks.push({ value: i, label: i });
+}
+
+function DisplayTimeOption({ displayTime, onDisplayTimeChange }) {
+  const handleDisplayTimeChange = (_, newTime) => onDisplayTimeChange(newTime);
+
+  return (
+    <>
+      <Typography id="word-display-time" variant="subtitle1" gutterBottom>
+        Display time for each word: {displayTime} second(s)
+      </Typography>
+      <Slider
+        aria-label="Display time"
+        aria-describedby="word-display-time"
+        min={0}
+        max={10}
+        step={null}
+        marks={displayTimeSliderMarks}
+        value={displayTime}
+        onChange={handleDisplayTimeChange}
+      />
+    </>
+  );
+}
+
+function StartButton({ onStart }) {
+  return (
+    <Button variant="contained" onClick={onStart}>
+      Start
+    </Button>
   );
 }
