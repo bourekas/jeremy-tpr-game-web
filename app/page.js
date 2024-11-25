5004b8ea9fdfbad7f588777f1d240238a138d0a3
bourekas <45741938+bourekas@users.noreply.github.com>
2024-11-25 14:27:03 +0200

    Add mock words to main page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,38 @@
 import TprGame from "./components/tpr-game/tpr-game";
 
+const words = [
+  {
+    word: "לָגַעַת",
+    imageSrc: "/words/לָגַעַת.jpg",
+    audioSrc: "/words/לָגַעַת.mp3",
+  },
+  {
+    word: "לְהַפְסִיק",
+    imageSrc: "/words/לְהַפְסִיק.jpg",
+    audioSrc: "/words/לְהַפְסִיק.mp3",
+  },
+  {
+    word: "לְהַקְשִׁיב",
+    imageSrc: "/words/לְהַקְשִׁיב.jpg",
+    audioSrc: "/words/לְהַקְשִׁיב.mp3",
+  },
+  {
+    word: "לְהַתְחִיל",
+    imageSrc: "/words/לְהַתְחִיל.jpg",
+    audioSrc: "/words/לְהַתְחִיל.mp3",
+  },
+  {
+    word: "לְהִתְקַשֵּׁר",
+    imageSrc: "/words/לְהִתְקַשֵּׁר.jpg",
+    audioSrc: "/words/לְהִתְקַשֵּׁר.mp3",
+  },
+  {
+    word: "לָזוּז",
+    imageSrc: "/words/לָזוּז.jpg",
+    audioSrc: "/words/לָזוּז.mp3",
+  },
+];
+
 export default function Home() {
-  return <TprGame />;
+  return <TprGame words={words} />;
 }
