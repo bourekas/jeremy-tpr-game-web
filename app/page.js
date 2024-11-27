a28a13928dd3b6a1b5b0ba210ff4e4e8368ac761
bourekas <45741938+bourekas@users.noreply.github.com>
2024-11-27 17:15:40 +0200

    Change images to webp format

--- app/page.js
+++ app/page.js
@@ -2,37 +2,17 @@ import Box from "@mui/material/Box";
 import TprGame from "./components/tpr-game/tpr-game";
 
 const words = [
-  {
-    word: "לָגַעַת",
-    imageSrc: "/words/לָגַעַת.jpg",
-    audioSrc: "/words/לָגַעַת.mp3",
-  },
-  {
-    word: "לְהַפְסִיק",
-    imageSrc: "/words/לְהַפְסִיק.jpg",
-    audioSrc: "/words/לְהַפְסִיק.mp3",
-  },
-  {
-    word: "לְהַקְשִׁיב",
-    imageSrc: "/words/לְהַקְשִׁיב.jpg",
-    audioSrc: "/words/לְהַקְשִׁיב.mp3",
-  },
-  {
-    word: "לְהַתְחִיל",
-    imageSrc: "/words/לְהַתְחִיל.jpg",
-    audioSrc: "/words/לְהַתְחִיל.mp3",
-  },
-  {
-    word: "לְהִתְקַשֵּׁר",
-    imageSrc: "/words/לְהִתְקַשֵּׁר.jpg",
-    audioSrc: "/words/לְהִתְקַשֵּׁר.mp3",
-  },
-  {
-    word: "לָזוּז",
-    imageSrc: "/words/לָזוּז.jpg",
-    audioSrc: "/words/לָזוּז.mp3",
-  },
-];
+  "לָגַעַת",
+  "לְהַפְסִיק",
+  "לְהַקְשִׁיב",
+  "לְהַתְחִיל",
+  "לְהִתְקַשֵּׁר",
+  "לָזוּז",
+].map((word) => ({
+  word,
+  imageSrc: `/words/${word}.webp`,
+  audioSrc: `/words/${word}.mp3`,
+}));
 
 export default function Home() {
   return (
