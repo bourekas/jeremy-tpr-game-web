12dfc87254adc2642ae2577ced8f46de1b0cfa4d
bourekas <45741938+bourekas@users.noreply.github.com>
2024-12-24 00:51:19 +0200

    Rewrite WordPlayer test

--- app/components/word-player/word-player.test.js
+++ app/components/word-player/word-player.test.js
@@ -19,6 +19,160 @@ it("calls onBackToSetup when clicking back to setup button", async () => {
   expect(onBackToSetup).toHaveBeenCalled();
 });
 
+it("calls stop control when clicking back to setup button", async () => {
+  const {
+    controls: { stop },
+    clickBackToSetupButton,
+  } = renderWordPlayer();
+
+  await clickBackToSetupButton();
+  expect(stop).toHaveBeenCalled();
+});
+
+it("forwards all props except onBackToSetup to useWordPlayer", () => {
+  const propsExceptOnBackToSetup = {
+    setup,
+    words,
+    initialIsPlaying: true,
+    initialWordIndex: 1,
+  };
+  const { useWordPlayer } = renderWordPlayer(propsExceptOnBackToSetup);
+
+  expect(useWordPlayer).toHaveBeenCalledWith(propsExceptOnBackToSetup);
+});
+
+it("forwards the returned word from useWordPlayer to WordContent", () => {
+  const word = words[0].word;
+  const imageSrc = words[0].imageSrc;
+  const { WordContent } = renderWordPlayer();
+
+  expect(WordContent.mock.lastCall[0]).toEqual(
+    expect.objectContaining({ word, imageSrc }),
+  );
+});
+
+it("forwards the returned isPlaying props from useWordPlayer to WordControls", () => {
+  const { WordControls, wordPlayerHookReturnValue } = renderWordPlayer();
+
+  expect(WordControls.mock.lastCall[0]).toHaveProperty(
+    "isPlaying",
+    wordPlayerHookReturnValue.isPlaying,
+  );
+});
+
+it("calls audio.play when WordControls calls onPlayAudio", () => {
+  const {
+    wordPlayerHookReturnValue: { audio },
+    WordControls,
+  } = renderWordPlayer();
+
+  act(() => {
+    const { onPlayAudio } = getControlHandlers(WordControls);
+    onPlayAudio();
+  });
+
+  expect(audio.play).toHaveBeenCalledTimes(1);
+});
+
+it("calls play control when WordControls calls onPlay", () => {
+  const {
+    wordPlayerHookReturnValue: {
+      controls: { play },
+    },
+    WordControls,
+  } = renderWordPlayer();
+
+  act(() => {
+    const { onPlay } = getControlHandlers(WordControls);
+    onPlay();
+  });
+
+  expect(play).toHaveBeenCalledTimes(1);
+});
+
+it("calls pause control when WordControls calls onPause", () => {
+  const {
+    wordPlayerHookReturnValue: {
+      controls: { pause },
+    },
+    WordControls,
+  } = renderWordPlayer();
+
+  act(() => {
+    const { onPause } = getControlHandlers(WordControls);
+    onPause();
+  });
+
+  expect(pause).toHaveBeenCalledTimes(1);
+});
+
+it("calls previous control when WordControls calls onPrevious", () => {
+  const {
+    wordPlayerHookReturnValue: {
+      controls: { previous },
+    },
+    WordControls,
+  } = renderWordPlayer();
+
+  act(() => {
+    const { onPrevious } = getControlHandlers(WordControls);
+    onPrevious();
+  });
+
+  expect(previous).toHaveBeenCalledTimes(1);
+});
+
+it("calls next control when WordControls calls onNext", () => {
+  const {
+    wordPlayerHookReturnValue: {
+      controls: { next },
+    },
+    WordControls,
+  } = renderWordPlayer();
+
+  act(() => {
+    const { onNext } = getControlHandlers(WordControls);
+    onNext();
+  });
+
+  expect(next).toHaveBeenCalledTimes(1);
+});
+
+it("calls stop control when WordControls calls onStop", () => {
+  const {
+    wordPlayerHookReturnValue: {
+      controls: { stop },
+    },
+    WordControls,
+  } = renderWordPlayer();
+
+  act(() => {
+    const { onStop } = getControlHandlers(WordControls);
+    onStop();
+  });
+
+  expect(stop).toHaveBeenCalledTimes(1);
+});
+
+it("calls onBackToSetup when WordControls calls onStop", () => {
+  const { onBackToSetup, WordControls } = renderWordPlayer();
+
+  act(() => {
+    const { onStop } = getControlHandlers(WordControls);
+    onStop();
+  });
+
+  expect(onBackToSetup).toHaveBeenCalledTimes(1);
+});
+
+function getControlHandlers(WordControls) {
+  return getProps(WordControls).controlHandlers;
+}
+
+function getProps(Mock) {
+  return Mock.mock.lastCall[0];
+}
+
 function renderWordPlayer(props = {}) {
   const onBackToSetup = jest.fn();
   const user = userEvent.setup();
@@ -26,9 +180,22 @@ function renderWordPlayer(props = {}) {
   const clickBackToSetupButton = () =>
     user.click(screen.getByRole("button", "Back to setup menu"));
 
-  const useWordPlayer = jest
-    .fn()
-    .mockReturnValue({ word: words[0], controls: { stop: jest.fn() } });
+  const controls = {
+    play: jest.fn(),
+    pause: jest.fn(),
+    previous: jest.fn(),
+    next: jest.fn(),
+    stop: jest.fn(),
+  };
+
+  const wordPlayerHookReturnValue = {
+    word: words[0],
+    audio: { play: jest.fn() },
+    isPlaying: true,
+    controls,
+  };
+  const useWordPlayer = jest.fn().mockReturnValue(wordPlayerHookReturnValue);
+
   const WordContent = jest.fn();
   const WordControls = jest.fn();
 
@@ -51,8 +218,9 @@ function renderWordPlayer(props = {}) {
     useWordPlayer,
     WordContent,
     WordControls,
+    controls,
+    wordPlayerHookReturnValue,
     onBackToSetup,
-    user,
     clickBackToSetupButton,
   };
 }
