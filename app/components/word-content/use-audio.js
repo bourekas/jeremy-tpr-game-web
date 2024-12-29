d7a599cd9953206484e172f98fd716f20b3c73bf
bourekas <45741938+bourekas@users.noreply.github.com>
2024-12-29 20:42:09 +0200

    Refactor useAudio

--- app/components/word-content/use-audio.js
+++ app/components/word-content/use-audio.js
@@ -1,16 +1,14 @@
-import { useEffect, useRef, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { SetupContext } from "@/app/contexts/setup";
 
 export default function useAudio(audio) {
-  const lastEndedAudioRef = useRef(null);
   const {
     setup: { isAutoPlayAudio: isAutoPlay },
   } = useContext(SetupContext);
 
   useEffect(() => {
-    if (!isAutoPlay || audio === lastEndedAudioRef.current) return;
+    if (!isAutoPlay) return;
 
-    audio.onended = () => (lastEndedAudioRef.current = audio);
     audio.play();
 
     return () => audio.pause();
