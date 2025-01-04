0fddc6206489393447551898cd28ae3552a8d646
bourekas <45741938+bourekas@users.noreply.github.com>
2025-01-04 21:53:21 +0200

    Add more controls to StoreGamePlaybackProvider

--- app/store/store-game-playback-provider/store-game-playback-provider.js
+++ app/store/store-game-playback-provider/store-game-playback-provider.js
@@ -1,12 +1,20 @@
 import { GamePlaybackContext } from "@/app/contexts";
-import { useSelector } from "@/lib/hooks";
+import { gameSlice } from "@/lib/game-slice";
+import { useDispatch, useSelector } from "@/lib/hooks";
 
 export default function StoreGamePlaybackProvider({ children }) {
   const index = useSelector((state) => state.game.playback.index);
   const isPlaying = useSelector((state) => state.game.playback.isPlaying);
+  const dispatch = useDispatch();
+  const controls = Object.fromEntries(
+    Object.entries(gameSlice.actions).map(([actionName, action]) => [
+      actionName,
+      () => dispatch(action()),
+    ]),
+  );
 
   return (
-    <GamePlaybackContext.Provider value={{ index, isPlaying }}>
+    <GamePlaybackContext.Provider value={{ index, isPlaying, controls }}>
       {children}
     </GamePlaybackContext.Provider>
   );
--- app/store/store-game-playback-provider/store-game-playback-provider.test.js
+++ app/store/store-game-playback-provider/store-game-playback-provider.test.js
@@ -1,18 +1,22 @@
-import { useContext } from "react";
+import { act, useContext } from "react";
 import { renderHook } from "@testing-library/react";
 import { GamePlaybackContext } from "@/app/contexts";
 import StoreProvider from "../store-provider/store-provider";
 import StoreGamePlaybackProvider from "./store-game-playback-provider";
 import { makeStore } from "@/lib/store";
+import { gameSlice } from "@/lib/game-slice";
 
-it.each([0, 4])("provides the playback index %d from the store", (index) => {
-  const { result } = renderStoreGamePlaybackProvider({ playback: { index } });
+it.each([0, 4])(
+  "provides the playback index from the store for index = %d",
+  (index) => {
+    const { result } = renderStoreGamePlaybackProvider({ playback: { index } });
 
-  expect(result.current.index).toBe(index);
-});
+    expect(result.current.index).toBe(index);
+  },
+);
 
 it.each([true, false])(
-  "provides if playback is playing from the store",
+  "provides isPlaying from the store for isPlaying = %s",
   (isPlaying) => {
     const { result } = renderStoreGamePlaybackProvider({
       playback: { isPlaying },
@@ -22,7 +26,62 @@ it.each([true, false])(
   },
 );
 
-function renderStoreGamePlaybackProvider({ playback }) {
+it("provides 'play' callback that dispatches the play action", () => {
+  const { result, reducer } = renderStoreGamePlaybackProvider();
+
+  act(() => result.current.controls.play());
+
+  expect(reducer).toHaveBeenLastCalledWith(
+    expect.anything(),
+    gameSlice.actions.play(),
+  );
+});
+
+it("provides 'pause' callback that dispatches the pause action", () => {
+  const { result, reducer } = renderStoreGamePlaybackProvider();
+
+  act(() => result.current.controls.pause());
+
+  expect(reducer).toHaveBeenLastCalledWith(
+    expect.anything(),
+    gameSlice.actions.pause(),
+  );
+});
+
+it("provides 'previous' callback that dispatches the previous action", () => {
+  const { result, reducer } = renderStoreGamePlaybackProvider();
+
+  act(() => result.current.controls.previous());
+
+  expect(reducer).toHaveBeenLastCalledWith(
+    expect.anything(),
+    gameSlice.actions.previous(),
+  );
+});
+
+it("provides 'next' callback that dispatches the next action", () => {
+  const { result, reducer } = renderStoreGamePlaybackProvider();
+
+  act(() => result.current.controls.next());
+
+  expect(reducer).toHaveBeenLastCalledWith(
+    expect.anything(),
+    gameSlice.actions.next(),
+  );
+});
+
+it("provides 'stop' callback that dispatches the stop action", () => {
+  const { result, reducer } = renderStoreGamePlaybackProvider();
+
+  act(() => result.current.controls.stop());
+
+  expect(reducer).toHaveBeenLastCalledWith(
+    expect.anything(),
+    gameSlice.actions.stop(),
+  );
+});
+
+function renderStoreGamePlaybackProvider({ playback = {} } = {}) {
   const initialState = { game: { playback } };
   const reducer = jest.fn((state) => state);
   const store = makeStore({ initialState, reducer });
